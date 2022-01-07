a46b3582af4479b35fb5c8bfc229ccdb00f9a3de
Nhat Tan Vu <onchua2006@gmail.com>
2022-01-07 12:19:10 -0800

    Fix Core Web Vitals issues to improve accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -110,12 +110,12 @@ function App() {
                             </li>
                         </ul>
                         <div className={"overlay " + (isMenuClosed ? "" : "mobile")}></div>
-                        <button id="menu-close-button" className={"flex " + (isMenuClosed ? "" : "mobile")} onClick={closeButtonClickHandler}>
+                        <button id="menu-close-button" aria-label="Close menu button" className={"flex " + (isMenuClosed ? "" : "mobile")} onClick={closeButtonClickHandler}>
                             <svg style={{ width: '0.896rem', height: '0.896rem', display: 'block' }} viewBox="0 0 14 15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fillRule="evenodd" /></svg>
                         </button>
                     </div>
                     <div className="right flex">
-                        <button id="view-cart-button" onClick={viewCartButtonClickHandler}>
+                        <button id="view-cart-button" aria-label="View cart button" onClick={viewCartButtonClickHandler}>
                             <span style={{ position: 'relative' }}>
                                 <svg className="cart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 20"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fillRule="nonzero" /></svg>
                                 {(totalNumOfProducts > 0) && <span className="cart-num-of-products">{totalNumOfProducts}</span>}
@@ -148,7 +148,7 @@ function App() {
                                         <span style={{ marginRight: '0.375rem' }}>${singleProductPrice}.00 x <span className="cart-num-of-products">{totalNumOfProducts}</span></span>
                                         <span style={{ color: 'var(--very-dark-blue)', fontWeight: 'bold' }} id="cart-total">{"$" + (totalNumOfProducts * singleProductPrice) + ".00"}</span>
                                     </div>
-                                    <button className="remove-cart-item-button flex" style={{ flex: 0, backgroundColor: 'transparent' }} onClick={removeCartItemClickHandler}>
+                                    <button className="remove-cart-item-button flex" aria-label="Remove cart item button" style={{ flex: 0, backgroundColor: 'transparent' }} onClick={removeCartItemClickHandler}>
                                         <svg className="remove-cart-item" width="14" height="16"><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" fillRule="nonzero" /></svg>
                                     </button>
                                 </div>
@@ -161,14 +161,14 @@ function App() {
             <main className="flex">
                 <div className="product-images">
                     <div className="desktop">
-                        <button id="main-image-button" onClick={mainImageButtonClickHandler}>
+                        <button id="main-image-button" aria-label="Main image button" onClick={mainImageButtonClickHandler}>
                             <img className="main-image" src={productImageUrls[mainImageIndex]} alt={productImageAlts[mainImageIndex]} />
                         </button>
                         <div className="thumbnails flex">
                             {productImageUrls.map((productImageUrl, index) => {
                                 let productImageAlt = productImageAlts[index];
                                 return (
-                                    <button key={index}
+                                    <button aria-label={"Thumbnail " + index + " button"} key={index}
                                         className={mainImageIndex === index ? "active" : ""}
                                         onClick={() => thumbnailClickHandler(index)}>
                                         <div className="overlay"></div>
@@ -180,10 +180,10 @@ function App() {
                     </div>
                     <div className="mobile">
                         <img className="main-image" src={productImageUrls[mainImageIndex]} alt={productImageAlts[mainImageIndex]} />
-                        <button id="main-previous-button" className="flex" onClick={mainPreviousButtonClickHandler}>
+                        <button id="main-previous-button" aria-label="Previous image button" className="flex" onClick={mainPreviousButtonClickHandler}>
                             <svg viewBox="0 0 12 18" xmlns="http://www.w3.org/2000/svg"><path d="M11 1 3 9l8 8" strokeWidth="3" fill="none" fillRule="evenodd" /></svg>
                         </button>
-                        <button id="main-next-button" className="flex" onClick={mainNextButtonClickHandler}>
+                        <button id="main-next-button" aria-label="Next image button" className="flex" onClick={mainNextButtonClickHandler}>
                             <svg viewBox="0 0 13 18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" strokeWidth="3" fill="none" fillRule="evenodd" /></svg>
                         </button>
                     </div>
@@ -201,15 +201,15 @@ function App() {
                     </div>
                     <div className="actions flex">
                         <div className="num-of-products-buttons flex">
-                            <button id="minus-button" onClick={reduceNumOfProductsAddedHandler}>
+                            <button id="minus-button" aria-label="Reduce number of products button" onClick={reduceNumOfProductsAddedHandler}>
                                 <svg width="12" height="4"><path d="M11.357 3.332A.641.641 0 0 0 12 2.69V.643A.641.641 0 0 0 11.357 0H.643A.641.641 0 0 0 0 .643v2.046c0 .357.287.643.643.643h10.714Z" fillRule="nonzero" /></svg>
                             </button>
                             <div id="num-of-products">{numOfProductsAdded}</div>
-                            <button id="plus-button" onClick={increaseNumOfProductsAddedHandler}>
+                            <button id="plus-button" aria-label="Increase number of products button" onClick={increaseNumOfProductsAddedHandler}>
                                 <svg width="12" height="12"><path d="M12 7.023V4.977a.641.641 0 0 0-.643-.643h-3.69V.643A.641.641 0 0 0 7.022 0H4.977a.641.641 0 0 0-.643.643v3.69H.643A.641.641 0 0 0 0 4.978v2.046c0 .356.287.643.643.643h3.69v3.691c0 .356.288.643.644.643h2.046a.641.641 0 0 0 .643-.643v-3.69h3.691A.641.641 0 0 0 12 7.022Z" fillRule="nonzero" /></svg>
                             </button>
                         </div>
-                        <button id="add-to-cart-button" className="flex" onClick={addToCartButtonClickHandler}>
+                        <button id="add-to-cart-button" aria-label="Add to cart button" className="flex" onClick={addToCartButtonClickHandler}>
                             <svg className="cart" style={{ width: '1rem', height: '1rem', fill: 'var(--white)' }} viewBox="0 0 22 20"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fillRule="nonzero" /></svg>
                             <div style={{ lineHeight: '1rem', fontWeight: 'bold' }}>Add to cart</div>
                         </button>
--- src/lightbox.jsx
+++ src/lightbox.jsx
@@ -75,7 +75,7 @@ class Lightbox extends React.Component {
                         {this.props.productImageUrls?.map((productImageUrl, index) => {
                             let productImageAlt = this.props.productImageAlts[index];
                             return (
-                                <button key={index}
+                                <button aria-label={"Thumbnail " + index + " button"} key={index}
                                     className={`${this.state.mainImageUrl === productImageUrl ? "active" : ""}`}
                                     onClick={() => this.handleThumbnailClick(index)}>
                                     <div className="overlay"></div>
@@ -84,13 +84,13 @@ class Lightbox extends React.Component {
                             );
                         })}
                     </div>
-                    <button id="close-button" className="flex" onClick={this.handleClose}>
+                    <button id="close-button" aria-label="Close lightbox button" className="flex" onClick={this.handleClose}>
                         <svg viewBox="0 0 14 15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fillRule="evenodd" /></svg>
                     </button>
-                    <button id="lightbox-previous-button" className="flex" onClick={this.handlePrevious}>
+                    <button id="lightbox-previous-button" aria-label="Previous lightbox image button" className="flex" onClick={this.handlePrevious}>
                         <svg viewBox="0 0 12 18" xmlns="http://www.w3.org/2000/svg"><path d="M11 1 3 9l8 8" strokeWidth="3" fill="none" fillRule="evenodd" /></svg>
                     </button>
-                    <button id="lightbox-next-button" className="flex" onClick={this.handleNext}>
+                    <button id="lightbox-next-button" aria-label="Next lightbox image button" className="flex" onClick={this.handleNext}>
                         <svg viewBox="0 0 13 18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" strokeWidth="3" fill="none" fillRule="evenodd" /></svg>
                     </button>
                 </div>
